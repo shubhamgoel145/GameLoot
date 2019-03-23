1790e88152eaddc5521bb20b37a6a84475ca38fe
Shubham Goel <shubhamsemailbox@gmail.com>
2019-03-23 15:47:38 +0000

    Initial Commit

---
+++ GameLoot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GameLoot
+//
+//  Created by shubham goel on 23/03/2019.
+//  Copyright © 2019 Creator Lab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
